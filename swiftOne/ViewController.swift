42b57e44edcbac49968ed7472f6c7161c52d7a36
Bogdan Adrian Galea <bogdan.galea84@gmail.com>
2017-05-01 18:54:18 +0200

    Initial Commit

---
+++ swiftOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftOne
+//
+//  Created by Bogdan A Galea on 01/05/17.
+//  Copyright (c) 2017 WhiteBelt Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
