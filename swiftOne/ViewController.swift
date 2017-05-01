c00313ed4bea7a32f11d2c52ebbb2b404674d5de
Bogdan Adrian Galea <bogdan.galea84@gmail.com>
2017-05-01 19:54:40 +0200

    my proj commit

--- swiftOne/ViewController.swift
+++ swiftOne/ViewController.swift
@@ -3,18 +3,40 @@
 //  swiftOne
 //
 //  Created by Bogdan A Galea on 01/05/17.
-//  Copyright (c) 2017 WhiteBelt Apps. All rights reserved.
+//  Copyright (c) 2017 WhiteBelt Apps. All rights reserved...
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLable: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func btnTap(sender: AnyObject) {
+        
+        
+        tapCount = tapCount + 1
+        print(tapCount)
+        
+        if tapCount >=  25 {
+            
+            theLable.text = "You have tapped 10 times"
+        }
+        
+    }
+    
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
